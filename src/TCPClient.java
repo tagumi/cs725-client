cf334587e15b161538099df678fe2648d22aa210
Tagumi <cmcjnz@gmail.com>
2018-07-29 22:30:39 +1200

    added TOBE command

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,9 +1,3 @@
-/**
- * Code has been adapted from Computer Networking: A Top-Down Approach Featuring
- * the Internet, second edition, copyright 1996-2002 J.F Kurose and K.W. Ross, 
- * All Rights Reserved.
- **/
-
 import java.io.*;
 import java.net.*;
 import java.util.Arrays;
